9fe5bc4df466e891f24a1600ef3cf1e3a62087b0
Michael Sederlin <37883908+michaelSederlin@users.noreply.github.com>
2025-09-02 13:45:27 +0200

    moving unit test dependencies and configuration to top level

---
+++ roundtrips/src/test/java/se/vti/roundtrips/TestRoundtrips.java
@@ -0,0 +1,13 @@
+package se.vti.roundtrips;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRoundtrips {
+    @Test
+    void testSomething() {
+        assertEquals(3, 2, "Failing");
+        // Your test code here
+    }
+}
