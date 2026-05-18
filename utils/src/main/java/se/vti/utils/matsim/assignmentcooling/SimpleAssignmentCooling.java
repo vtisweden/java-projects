0592fa745271c05ef4ca2c83ac185ce9cf24d8d3
GunnarF <GunnarF@GUNNARF-ZB14G11.vti.se>
2026-05-18 09:35:58 +0200

    published simple assignment cooling example

---
+++ utils/src/main/java/se/vti/utils/matsim/assignmentcooling/SimpleAssignmentCooling.java
@@ -0,0 +1,90 @@
+package se.vti.utils.matsim.assignmentcooling;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.config.Config;
+import org.matsim.core.controler.events.IterationStartsEvent;
+import org.matsim.core.controler.events.StartupEvent;
+import org.matsim.core.controler.listener.IterationStartsListener;
+import org.matsim.core.controler.listener.StartupListener;
+import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.replanning.selectors.ExpBetaPlanSelector;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+/**
+ * @author GunnarF
+ */
+@Singleton
+public class SimpleAssignmentCooling implements IterationStartsListener, StartupListener {
+
+	private static final Logger log = LogManager.getLogger(SimpleAssignmentCooling.class);
+
+	private Set<String> allSubpopulations = null;
+
+	@Inject
+	public SimpleAssignmentCooling(Config config) {
+	}
+
+	@Override
+	public void notifyStartup(StartupEvent event) {
+		this.allSubpopulations = new LinkedHashSet<>();
+		for (Person person : event.getServices().getScenario().getPopulation().getPersons().values()) {
+			this.allSubpopulations.add(PopulationUtils.getSubpopulation(person));
+		}
+		log.info("Identified subpopulations: " + this.allSubpopulations);
+	}
+
+	private double computeRate(double initialRate, int iteration, double iterationExponent) {
+		return initialRate / Math.pow(1.0 + iteration, iterationExponent);
+	}
+
+	@Override
+	public void notifyIterationStarts(IterationStartsEvent event) {
+		int iteration = event.getIteration() - event.getServices().getConfig().controller().getFirstIteration();
+		if (iteration >= 0) {
+
+			final double baseInnovationRate = this.computeRate(1.0, iteration, 0.5);
+			log.info("Setting base innovation rate to " + baseInnovationRate);
+
+			var strategyManager = event.getServices().getStrategyManager();
+			for (String subpopulation : this.allSubpopulations) {
+
+				int numberOfExpBetaPlanSelectorStrategies = 0;
+				int numberOfInnovationStrategies = 0;
+				for (var strategy : strategyManager.getStrategies(subpopulation)) {
+					String strategyName = strategy.toString();
+					if (ExpBetaPlanSelector.class.getSimpleName().toString().equals(strategyName)) {
+						numberOfExpBetaPlanSelectorStrategies++;
+					} else {
+						numberOfInnovationStrategies++;
+					}
+				}
+				assert (numberOfExpBetaPlanSelectorStrategies > 0);
+				assert (numberOfInnovationStrategies > 0);
+
+				final double innovationRate = Math.min(1.0, numberOfInnovationStrategies * baseInnovationRate);
+				final double selectionRate = 1.0 - innovationRate;
+				log.info("Subpoulation: " + subpopulation);
+				log.info("  Setting innovation rate rate to " + innovationRate);
+				log.info("  Setting selection rate to " + selectionRate);
+
+				for (var strategy : strategyManager.getStrategies(subpopulation)) {
+					String strategyName = strategy.toString();
+					double weight;
+					if (ExpBetaPlanSelector.class.getSimpleName().toString().equals(strategyName)) {
+						weight = selectionRate / numberOfExpBetaPlanSelectorStrategies;
+					} else {
+						weight = innovationRate / numberOfInnovationStrategies;
+					}
+					strategyManager.changeWeightOfStrategy(strategy, subpopulation, weight);
+				}
+			}
+		}
+	}
+}
---
+++ utils/src/main/java/se/vti/utils/matsim/assignmentcooling/TestSimpleAssignmentCoolingOslo.java
@@ -0,0 +1,62 @@
+/**
+ * se.vti.atap
+ * 
+ * Copyright (C) 2025 by Gunnar Flötteröd (VTI, LiU).
+ * 
+ * VTI = Swedish National Road and Transport Institute
+ * LiU = Linköping University, Sweden
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>. See also COPYING and WARRANTY file.
+ */
+package se.vti.utils.matsim.assignmentcooling;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.scenario.ScenarioUtils;
+
+/**
+ * Berlin scenario used in the article: <i>"A simulation heuristic for traveler-
+ * and vehicle-discrete dynamic traffic assignment"</i>.
+ *
+ * <b>Scenario data:</b>
+ * https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/zz_archive/2014-08-01_car_1pct
+ * 
+ * @author Gunnar Flötteröd
+ */
+
+public class TestSimpleAssignmentCoolingOslo {
+
+	public static void main(String[] args) {
+
+		String configFileName = "./src/test/resources/se/vti/utils/matsim/assignmentcooling/oslo_config_cooling_example.xml";
+
+		Config config = ConfigUtils.loadConfig(configFileName);
+		config.controller().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				this.addControlerListenerBinding().to(SimpleAssignmentCooling.class);
+			}
+		});
+
+		controler.run();
+	}
+
+}
